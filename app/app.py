efe0bfd26294c9baa1d57bc2250ecd5a5da4e4af
Evans <manotievance@gmail.com>
2024-01-18 00:02:02 +0300

    corrected code destination

--- app/app.py
+++ app/app.py
@@ -31,22 +31,6 @@ def home():
 
 #     return 'Hero added'
 
-#heromodel
-class Hero(db.Model):
-    __tablename__ = "heroes"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    hero_name = db.Column(db.String(100), nullable=False)
-    powers = db.Column(db.String(100), nullable=False)
-
-
-#powermodel
-class Power(db.model):
-    __tablename__="powers"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(100), nullable=False)
-
 
 if __name__ == '__main__':
     app.run(port=3000,debug = True )
